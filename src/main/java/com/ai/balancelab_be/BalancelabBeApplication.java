053086c45d7f4fdd667efa56c4cc8ff8fc68cd19
hsm9361 <hsm9361@naver.com>
2025-04-03 09:13:45 +0900

    init

---
+++ src/main/java/com/ai/balancelab_be/BalancelabBeApplication.java
@@ -0,0 +1,13 @@
+package com.ai.balancelab_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancelabBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BalancelabBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ai/balancelab_be/BalancelabBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ai.balancelab_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BalancelabBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
