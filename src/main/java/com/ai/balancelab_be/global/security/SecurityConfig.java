37156233a64b360c6ac21d07af1d9a930b459cb6
Han minyoung <han443370@gmail.com>
2025-04-10 15:05:56 +0900

    SecurityConfig 허용 url 추가

--- src/main/java/com/ai/balancelab_be/global/security/SecurityConfig.java
+++ src/main/java/com/ai/balancelab_be/global/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/oauth2/**", "/auth/**").permitAll()
+                        .requestMatchers("/login", "/oauth2/**", "/auth/**", "/diet-analysis/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
