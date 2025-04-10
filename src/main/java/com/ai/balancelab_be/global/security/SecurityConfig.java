ddb5292bcd23848d7d8dbe45d9a796edf57cc9c5
hsm9361 <hsm9361@naver.com>
2025-04-10 15:17:32 +0900

    시큐리티 추가

--- src/main/java/com/ai/balancelab_be/global/security/SecurityConfig.java
+++ src/main/java/com/ai/balancelab_be/global/security/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/oauth2/**", "/auth/**", "/diet-analysis/**").permitAll()
+                        .requestMatchers("/login", "/oauth2/**", "/auth/**", "/diet-analysis/**","/image-analysis/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
