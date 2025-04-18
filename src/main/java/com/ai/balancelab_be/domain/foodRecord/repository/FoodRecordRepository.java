70bced0ee38e8f3fd9dc15702ba149532609f04d
Han minyoung <han443370@gmail.com>
2025-04-18 17:41:22 +0900

    월간캘린더 데이터가 있는 날짜는 달력에 점으로 표시되게 수정

--- src/main/java/com/ai/balancelab_be/domain/foodRecord/repository/FoodRecordRepository.java
+++ src/main/java/com/ai/balancelab_be/domain/foodRecord/repository/FoodRecordRepository.java
@@ -1,7 +1,10 @@
 package com.ai.balancelab_be.domain.foodRecord.repository;
 
+import com.ai.balancelab_be.domain.foodRecord.dto.FoodRecordCountDto;
 import com.ai.balancelab_be.domain.foodRecord.entity.FoodRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -12,4 +15,15 @@ public interface FoodRecordRepository extends JpaRepository<FoodRecordEntity, Lo
     List<FoodRecordEntity> findByMemberIdAndGroupId(Long memberId, String groupId);
     List<FoodRecordEntity> findByConsumedDate(LocalDate date);
     List<FoodRecordEntity> findByMemberIdAndConsumedDate(Long memberId, LocalDateTime consumedDate);
+    List<FoodRecordEntity> findByMemberIdAndConsumedDateBetween(Long memberId, LocalDateTime startDate, LocalDateTime endDate);
+    @Query("SELECT new com.ai.balancelab_be.domain.foodRecord.dto.FoodRecordCountDto(COUNT(f.consumedDate), f.consumedDate) " +
+            "FROM FoodRecordEntity f " +
+            "WHERE f.memberId = :memberId " +
+            "AND f.consumedDate BETWEEN :startDate AND :endDate " +
+            "GROUP BY f.consumedDate")
+    List<FoodRecordCountDto> findFoodRecordCountByDateRange(
+            @Param("memberId") Long memberId,
+            @Param("startDate") LocalDateTime startDate,
+            @Param("endDate") LocalDateTime endDate
+    );
 }
--- src/main/java/com/ai/balancelab_be/domain/foodRecord/service/FoodRecordService.java
+++ src/main/java/com/ai/balancelab_be/domain/foodRecord/service/FoodRecordService.java
@@ -1,5 +1,6 @@
 package com.ai.balancelab_be.domain.foodRecord.service;
 
+import com.ai.balancelab_be.domain.foodRecord.dto.FoodRecordCountDto;
 import com.ai.balancelab_be.domain.foodRecord.dto.FoodRecordDto;
 
 import java.time.LocalDate;
@@ -14,4 +15,6 @@ public interface FoodRecordService {
     FoodRecordDto updateFoodRecord(Long id, FoodRecordDto foodRecordDto);
     void deleteFoodRecord(Long id);
     List<FoodRecordDto> findByMemberIdAndConsumedDate(Long memberId, LocalDateTime consumedDate);
+    List<FoodRecordCountDto> getFoodRecordCounts(Long memberId, LocalDateTime startDate, LocalDateTime endDate);
+
 }
